22ac19c6be3ce0a467e4f872b94d3ada4321a89b
Kyle Cesare <kcesare@gmail.com>
2026-07-24 13:08:58 -0600

    Set SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject io.github.kylc/minitui "0.1.0"
+(defproject io.github.kylc/minitui "0.2.0-SNAPSHOT"
   :description "Create terminal user interfaces in jank"
   :url "https://github.com/kylc/minitui"
   :license {:name "MPL 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.jank-lang.commons/ftxui-sys "2026.06-1"]]
   :plugins [[org.jank-lang/lein-jank "2026.07-3"]]
   :middleware [leiningen.jank/middleware]
